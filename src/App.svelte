f4537c58423f920af5e10902ecb10363b90a2e87
garricknorthover <garricknorthover@gmail.com>
2020-05-06 21:01:21 +1200

    basic front, no backend yet

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let name;
+  let count = 0;
+  function handle() {
+    count++;
+  }
+</script>
+
+<style>``
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <button on:click={handle}>{count}</button>
+</main>
